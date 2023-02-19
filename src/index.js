39834c3a32ff7dd24fd98eaa7f62236182d9c316
pulsehexican <123429695+pulsehexican@users.noreply.github.com>
2023-02-19 18:19:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function Formular() {
     return (
       <div className="Formular">
           <p>
-            Test <code>http://go.HEX.com</code> and stake 5555 !!!
+            Test <a>http://go.HEX.com</a> and stake 5555 !!!
           </p>
       </div>
     );
@@ -36,8 +36,8 @@ var testing = Formular();
 // ReactDOM.render(navbar, document.getElementById("root"))
 //ReactDOM.createRoot(document.getElementById("content")).render(navbar)
 
-const content = ReactDOM.createRoot(document.getElementById("content"));
-content.render(navbar)
+//const content = ReactDOM.createRoot(document.getElementById("content"));
+//content.render(navbar)
 
 const footer = ReactDOM.createRoot(document.getElementById("footer"));
 footer.render(testing)

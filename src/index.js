4934a2fbf64177f35ba65c480742c22ee701406e
pulsehexican <123429695+pulsehexican@users.noreply.github.com>
2023-02-19 15:40:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-  <p>test paragraph original</p>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-ReactDOM.render(<App/>,document.getElementById('content'));
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
 reportWebVitals(sendToVercelAnalytics);

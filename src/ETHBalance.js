263da533af714727da364c3a9a46063c8c8704a4
pulsehexican <pulsechain555@gmail.com>
2023-02-20 10:27:44 +0100

    testowy

--- src/ETHBalance.js
+++ src/ETHBalance.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-/*import Web3 from 'web3';
+import Web3 from 'web3';
 
 function ETHBalance() {
   const [ethAddress, setEthAddress] = useState('');
@@ -30,4 +30,4 @@ function ETHBalance() {
 }
 
 export default ETHBalance;
-*/
+
